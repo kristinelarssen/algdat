7576087ec6a3ffe16e33321f01242a5e1f8cc47a
Kristine Larssen <krlarss@stud.ntnu.no>
2019-09-13 00:16:13 +0200

    init commit

---
+++ oving2/linkedlist.jl
@@ -0,0 +1,40 @@
+### LINKED LIST
+
+mutable struct Node
+    next::Union{Node, Nothing} # next kan peke på et Node-objekt eller ha verdien nothing.
+    value::Int
+end
+
+function createlinkedlist(length)
+    # Creates the list starting from the last element
+    # This is done so the last element we generate is the head
+    child = nothing
+    node = nothing
+
+    for i in 1:length
+        node = Node(child, rand(1:800))
+        child = node
+    end
+    return node
+end
+
+
+function findindexinlist(linkedlist, index)
+   head = linkedlist
+   for i in 1:index
+        if i == index
+            return head.value
+        elseif head.next == nothing
+            return -1
+        else
+            head = head.next
+        end
+    end
+end
+
+
+list1 = createlinkedlist(6)
+println(findindexinlist(list1, 4)) #should return value at index 4
+println(findindexinlist(list1, 7)) #should return -1
+
+
---
+++ oving2/stack.jl
@@ -0,0 +1,12 @@
+### STACK
+function reverseandlimit(array, maxnumber)
+    s = []
+    for i in length(array):-1:1
+        push!(s, array[i])
+    end
+    
+end
+
+list = [3, 2, 1]
+
+reverseandlimit(list, 3)
